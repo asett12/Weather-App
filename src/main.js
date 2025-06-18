b4f689a6229fb187732ecefef00b05d427d593c1
asett12 <158172308+asett12@users.noreply.github.com>
2025-06-18 16:54:32 +0700

    Final weatherapp simple version

--- src/main.js
+++ src/main.js
@@ -1,24 +1,46 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const apiKey = "b39f01a4b3653828f9b641737f6a9bf9";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const weatherIcon = document.querySelector(".weatherIcon img");
+const searchBox = document.querySelector(".searchBox");
+const searchBtn = document.querySelector(".searchBtn");
 
-setupCounter(document.querySelector('#counter'))
+async function checkWeather(cityName){
+    const response = await fetch(apiUrl + cityName + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    const weatherCondition = data.weather[0].main;
+    console.log(weatherCondition.src)
+
+    if ( weatherCondition === "Clouds"){
+        weatherIcon.src = "/images/cloudy-day.png"
+    } else if (weatherCondition === "Clear"){
+        weatherIcon.src = "/images/clear-sky.png"
+    } else if (weatherCondition === "Rain"){
+        weatherIcon.src = "/images/heavy-rain.png"
+    } else if (weatherCondition === "Drizzle"){
+        weatherIcon.src = "/images/drizzle.png"
+    } else if (weatherCondition === "Mist"){
+        weatherIcon.src = "/images/fog.png"
+    }
+
+    document.querySelector(".temperature").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".cityName").innerHTML = data.name;
+    document.querySelector(".humidityPercentage").innerHTML = data.main.humidity + "%";
+    document.querySelector(".windSpeed").innerHTML = data.wind.speed + " km/h";
+
+    console.log(data);
+};
+
+searchBtn.addEventListener("click", ()=> {
+    const cityToSearch = searchBox.value;
+    checkWeather(cityToSearch);
+});
+
+searchBox.addEventListener("keydown", (event)=>{
+    if (event.key === "Enter"){
+        const cityToSearch = searchBox.value;
+        checkWeather(cityToSearch);
+    };
+});
